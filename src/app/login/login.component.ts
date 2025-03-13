caf5eabefb0f47f8b25f1e89c1318b583233b64a
Mithilesh Kumar <69746545+MithileshKumar-IITRPR@users.noreply.github.com>
2025-03-14 01:13:10 +0530

    Initial project setup and login comp commit

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+  name: string = '';
+
+  constructor(private router: Router) {}
+
+  login() {
+    if (this.name.trim()) {
+        localStorage.setItem('user', JSON.stringify(this.name));
+    }
+  }
+}
